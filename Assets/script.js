3049bde1b57c4170f7ebec294899b094262c29ac
emma@beinproximity.com <emma@beinproximity.com>
2022-01-19 15:16:04 -0700

    initial commit

---
+++ Assets/script.js
@@ -0,0 +1,87 @@
+// AS A coding boot camp student
+// I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
+// SO THAT I can gauge my progress compared to my peers
+
+// GIVEN I am taking a code quiz
+// WHEN I click the start button
+// THEN a timer starts and I am presented with a question
+// WHEN I answer a question
+// THEN I am presented with another question
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+// WHEN the game is over
+// THEN I can save my initials and my score
+
+// Assign query selector variables
+var startQuiz = document.querySelector(".start-quiz");
+
+//Assign other variables
+
+    // Emptry array for high scores to be pushed in to from local storage
+    var storedHighScores = []
+
+    // Connects timeEl with timer in html
+    var timeEl = document.querySelector(".timer");
+
+    // Sets the seconds in the game for setTime function
+    var secondsLeft = 5;
+
+// Assign variables to array for quiz questions
+var quizQuestions = [
+    {question: "What is the correct way to console log something?",
+    answers: {
+        a: "consoleLog()",
+        b: "console.log()",
+        c: "console.log[]"
+    },
+    correctAnswer: "b"
+    },
+    {question: "If you type the following code in the console window, what result will you get? \n3 > 2 > 1 === false;",
+    answers: {
+        a: "true",
+        b: "false",
+    },
+    correctAnswer: "a"
+    },
+]
+
+
+// Create function to begin quiz
+function begin(){
+    // Call function to begin timer
+    setTime();
+    console.log(quizQuestions)
+
+}
+
+// Timer function
+function setTime() {
+    // Sets interval in variable
+    var timerInterval = setInterval(function() {
+      secondsLeft--;
+      timeEl.textContent = secondsLeft + " seconds left.";
+
+      if(secondsLeft === 0) {
+        // Stops execution of action at set interval
+        clearInterval(timerInterval);
+      }
+  
+    }, 1000);
+  }
+
+// Call functions
+
+
+// Get item/set item to store info to local storage
+
+
+// Get items from local storage - push to empty array
+
+
+// Display empty array in to high scores
+
+
+// Create event listeners for start-quiz button
+startQuiz.addEventListener("click", begin);
